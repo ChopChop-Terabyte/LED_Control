318022137e89167d4cfdc320a4e9027c2450938a
kali_lapbot <ndt0012@lapbotlinux.lapbotkali.com>
2025-01-13 01:30:21 +0700

    Test 2

---
+++ build/esp-idf/mbedtls/mbedtls/library/error.c
@@ -0,0 +1,1 @@
+/home/ndt0012/esp/v5.3.1/esp-idf/components/mbedtls/mbedtls/library/error.c
---
+++ main/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
+
+#define LED GPIO_NUM_2
+
+void LED_config() {
+	gpio_reset_pin(LED);
+	gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+
+	while(1) {
+		gpio_set_level(LED, 0);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		gpio_set_level(LED, 1);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+}
+
+void app_main(void)
+{
+    LED_config();
+}
